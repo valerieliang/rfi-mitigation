c8d9b8e9d7290058b158560b6327a6dd1bbb1e5e
Valerie Liang <143350210+valerieliang@users.noreply.github.com>
2026-06-23 15:31:04 -0700

    inspect generated CPIs

---
+++ inspect_h5.py
@@ -0,0 +1,25 @@
+import h5py
+import numpy as np
+from matplotlib import pyplot as plt
+
+# Use 'r' before the string to fix the SyntaxWarning invalid escape sequence
+with h5py.File(r'data\high_power\single_tone\image_0.h5', 'r') as f:
+    dataset = f['/cpi_0_0']
+    
+    # Read the data directly
+    matrix = dataset[:]
+
+# Print your results
+print("Matrix Shape:", matrix.shape)
+print("Data Type:", matrix.dtype)
+#print(matrix)
+
+num_rows, num_cols = matrix.shape
+
+matrix_pwr_db = 10 * np.log10(np.abs(matrix)**2/num_cols)
+
+plt.figure()
+plt.imshow(matrix_pwr_db, aspect='auto', cmap='grey', origin='lower')
+plt.colorbar(label='Power (dB)')
+plt.show()
+
