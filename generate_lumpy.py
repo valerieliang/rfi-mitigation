10f08a00426f88891cd6d28c763080aaef496f5a
Valerie Liang <143350210+valerieliang@users.noreply.github.com>
2026-07-17 10:00:58 -0700

    upload vienna results

--- generate_lumpy.py
+++
@@ -1,9 +0,0 @@
-"""
-generate_lumpy.py
-
-- use the anomaly detection model to flag 100% clean data --> the hope is that there are minimal deviations from a curve = as close to ideal clean sample as possible
-- generate 1 to 2 pulses of low power ([1,4] JSR) RFI in the data --> map to "clean" labelled data 
-
-
-
-"""
